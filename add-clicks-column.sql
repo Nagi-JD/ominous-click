6b7ee76f21a4810ce0e9847caa3377b536d677ba
TonNom <deurweilher.johann@gmail.com>
2025-10-27 18:58:36 +0100

    Clean up project: professional README, remove temp files

--- add-clicks-column.sql
+++
@@ -1,6 +0,0 @@
--- Add total_clicks column to scores table
-ALTER TABLE scores ADD COLUMN IF NOT EXISTS total_clicks BIGINT DEFAULT 0;
-
--- Create index for fast queries by clicks
-CREATE INDEX IF NOT EXISTS idx_total_clicks ON scores(total_clicks DESC);
-
